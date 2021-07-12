62c033974c41e780ed5c34d676e7bd0cb77aef15
Iosif <Joseph.solntseph@gmail.com>
2021-07-12 12:13:12 +0300

    изменение данных из личного кабинета

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.contrib import auth, messages
 from django.urls import reverse
-
+from baskets.models import Basket
 
 # Create your views here.
 def login(request):
@@ -37,12 +37,26 @@ def registration(request):
         form = UserRegistrationForm()
 
     context = {
-        'title': 'GeekShop - Регистрация', 'form': form}
+        'title': 'GeekShop - Регистрация',
+        'form': form,
+
+    }
     return render(request, 'users/registration.html', context)
 
 
 def profile(request):
-    context = {'title': 'GeekShop - Личный кабинет'}
+    user = request.user
+    if request.method == 'POST':
+        form = UserProfileForm(instance=user,files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=user)
+    context = {'title': 'GeekShop - Личный кабинет',
+               'form': form,
+               'baskets': Basket.objects.filter(user=user),
+               }
     return render(request, 'users/profile.html', context)
 
 
